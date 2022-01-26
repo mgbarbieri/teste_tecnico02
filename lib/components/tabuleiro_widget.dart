e476c32d13603f56199fb13ccc507c525b787f23
Matheus Barbieri <mgbarbieri20@gmail.com>
2022-01-26 19:27:50 -0300

    refactor: dados do tabuleiro para widget

--- lib/components/tabuleiro_widget.dart
+++ lib/components/tabuleiro_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:teste_tecnico02/components/campo_widget.dart';
 import 'package:teste_tecnico02/models/tabuleiro.dart';
@@ -9,14 +11,227 @@ class TabuleiroWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: tabuleiro.colunas,
-      children: tabuleiro.campos
-          .map((c) => CampoWidget(
-                campo: c,
-                cor: Color(int.parse(c.cor)),
-              ))
-          .toList(),
+    return Stack(
+      children: [
+        Container(
+          height: 480,
+          width: 480,
+          child: GridView.count(
+            crossAxisCount: tabuleiro.colunas,
+            children: tabuleiro.campos
+                .map((c) => CampoWidget(
+                      campo: c,
+                      cor: Color(int.parse(c.cor)),
+                    ))
+                .toList(),
+          ),
+        ),
+        Positioned(
+          left: 280,
+          bottom: 10,
+          child: Transform.rotate(
+            angle: 270,
+            child: Image.asset(
+              'assets/ladder.png',
+              scale: 0.2,
+              width: 60,
+              height: 60,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 235,
+          bottom: 60,
+          child: Image.asset(
+            'assets/ladder.png',
+            scale: 0.2,
+            width: 60,
+            height: 60,
+          ),
+        ),
+        Positioned(
+          left: 85,
+          bottom: 10,
+          child: Transform.rotate(
+            angle: pi / 15,
+            child: Image.asset(
+              'assets/ladder2.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 160,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 395,
+          bottom: -5,
+          child: Transform.rotate(
+            angle: pi / 4,
+            child: Image.asset(
+              'assets/ladder2.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 140,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 40,
+          bottom: 100,
+          child: Transform.rotate(
+            angle: pi / 6,
+            child: Image.asset(
+              'assets/ladder2.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 120,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 190,
+          bottom: 150,
+          child: Transform.rotate(
+            angle: pi / 1.5,
+            child: Image.asset(
+              'assets/ladder.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 70,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 250,
+          bottom: 80,
+          child: Transform.rotate(
+            angle: -pi / 5.5,
+            child: Image.asset(
+              'assets/ladder2.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 360,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 90,
+          bottom: 200,
+          child: Transform.rotate(
+            angle: -pi / 2.5,
+            child: Image.asset(
+              'assets/ladder2.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 170,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 110,
+          top: 30,
+          child: Transform.rotate(
+            angle: pi / 30,
+            child: Image.asset(
+              'assets/ladder2.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 100,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 10,
+          top: 20,
+          child: Transform.rotate(
+            angle: -pi / 30,
+            child: Image.asset(
+              'assets/ladder2.png',
+              fit: BoxFit.fill,
+              width: 20,
+              height: 100,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 150,
+          top: 20,
+          child: Transform.rotate(
+            angle: -pi / 30,
+            child: Image.asset(
+              'assets/ladder.png',
+              fit: BoxFit.fill,
+              width: 30,
+              height: 60,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 30,
+          top: 20,
+          child: Image.asset('assets/snake1.png'),
+        ),
+        Positioned(
+          left: 350,
+          top: 70,
+          child: Image.asset('assets/snake2.png'),
+        ),
+        Positioned(
+          left: 350,
+          top: 20,
+          child: Image.asset('assets/snake3.png'),
+        ),
+        Positioned(
+          left: 230,
+          top: 20,
+          child: Transform.rotate(
+            angle: -pi / 4,
+            child: Image.asset(
+              'assets/snake5.png',
+              fit: BoxFit.fill,
+              height: 90,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 270,
+          top: 120,
+          child: Image.asset('assets/snake6.png'),
+        ),
+        Positioned(
+          left: 30,
+          top: 160,
+          child: Image.asset('assets/snake7.png'),
+        ),
+        Positioned(
+          left: 40,
+          top: 160,
+          child: Image.asset(
+            'assets/snake8.png',
+            fit: BoxFit.fill,
+            height: 250,
+          ),
+        ),
+        Positioned(
+          left: 180,
+          top: 260,
+          child: Image.asset('assets/snake9.png'),
+        ),
+        Positioned(
+          left: 190,
+          bottom: 20,
+          child: Image.asset('assets/snake10.png'),
+        ),
+        Positioned(
+          left: 370,
+          bottom: 70,
+          child: Image.asset(
+            'assets/snake11.png',
+            fit: BoxFit.fill,
+            height: 150,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/cobras_escadas.dart
+++ lib/screens/cobras_escadas.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:teste_tecnico02/components/tabuleiro_widget.dart';
 import 'package:teste_tecnico02/models/campo.dart';
+import 'package:teste_tecnico02/models/jogador.dart';
 import 'package:teste_tecnico02/models/tabuleiro.dart';
 
 class CobrasEscadas extends StatefulWidget {
@@ -12,17 +13,24 @@ class CobrasEscadas extends StatefulWidget {
 
   @override
   _CobrasEscadasState createState() => _CobrasEscadasState();
-
-  void jogar(int dado1, int dado2) {}
 }
 
 class _CobrasEscadasState extends State<CobrasEscadas> {
   List _data = [];
+  //Jogador jogador1, jogador2;
 
   @override
   initState() {
     super.initState();
     loadJsonData();
+  }
+
+  int jogarDado() {
+    return Random().nextInt(5) + 1;
+  }
+
+  void jogar(int dado1, int dado2) {
+    setState(() {});
   }
 
   Future<String> loadJsonData() async {
@@ -53,221 +61,8 @@ class _CobrasEscadasState extends State<CobrasEscadas> {
                   child: CircularProgressIndicator(),
                 );
               } else {
-                return Stack(
-                  children: [
-                    Container(
-                      height: 480,
-                      width: 480,
-                      child: TabuleiroWidget(
-                        tabuleiro: Tabuleiro(10, 10, _data as List<Campo>),
-                      ),
-                    ),
-                    Positioned(
-                      left: 280,
-                      bottom: 10,
-                      child: Transform.rotate(
-                        angle: 270,
-                        child: Image.asset(
-                          'assets/ladder.png',
-                          scale: 0.2,
-                          width: 60,
-                          height: 60,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 235,
-                      bottom: 60,
-                      child: Image.asset(
-                        'assets/ladder.png',
-                        scale: 0.2,
-                        width: 60,
-                        height: 60,
-                      ),
-                    ),
-                    Positioned(
-                      left: 85,
-                      bottom: 10,
-                      child: Transform.rotate(
-                        angle: pi / 15,
-                        child: Image.asset(
-                          'assets/ladder2.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 160,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 395,
-                      bottom: -5,
-                      child: Transform.rotate(
-                        angle: pi / 4,
-                        child: Image.asset(
-                          'assets/ladder2.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 140,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 40,
-                      bottom: 100,
-                      child: Transform.rotate(
-                        angle: pi / 6,
-                        child: Image.asset(
-                          'assets/ladder2.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 120,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 190,
-                      bottom: 150,
-                      child: Transform.rotate(
-                        angle: pi / 1.5,
-                        child: Image.asset(
-                          'assets/ladder.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 70,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 250,
-                      bottom: 80,
-                      child: Transform.rotate(
-                        angle: -pi / 5.5,
-                        child: Image.asset(
-                          'assets/ladder2.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 360,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 90,
-                      bottom: 200,
-                      child: Transform.rotate(
-                        angle: -pi / 2.5,
-                        child: Image.asset(
-                          'assets/ladder2.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 170,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 110,
-                      top: 30,
-                      child: Transform.rotate(
-                        angle: pi / 30,
-                        child: Image.asset(
-                          'assets/ladder2.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 100,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 10,
-                      top: 20,
-                      child: Transform.rotate(
-                        angle: -pi / 30,
-                        child: Image.asset(
-                          'assets/ladder2.png',
-                          fit: BoxFit.fill,
-                          width: 20,
-                          height: 100,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 150,
-                      top: 20,
-                      child: Transform.rotate(
-                        angle: -pi / 30,
-                        child: Image.asset(
-                          'assets/ladder.png',
-                          fit: BoxFit.fill,
-                          width: 30,
-                          height: 60,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 30,
-                      top: 20,
-                      child: Image.asset('assets/snake1.png'),
-                    ),
-                    Positioned(
-                      left: 350,
-                      top: 70,
-                      child: Image.asset('assets/snake2.png'),
-                    ),
-                    Positioned(
-                      left: 350,
-                      top: 20,
-                      child: Image.asset('assets/snake3.png'),
-                    ),
-                    Positioned(
-                      left: 230,
-                      top: 20,
-                      child: Transform.rotate(
-                        angle: -pi / 4,
-                        child: Image.asset(
-                          'assets/snake5.png',
-                          fit: BoxFit.fill,
-                          height: 90,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 270,
-                      top: 120,
-                      child: Image.asset('assets/snake6.png'),
-                    ),
-                    Positioned(
-                      left: 30,
-                      top: 160,
-                      child: Image.asset('assets/snake7.png'),
-                    ),
-                    Positioned(
-                      left: 40,
-                      top: 160,
-                      child: Image.asset(
-                        'assets/snake8.png',
-                        fit: BoxFit.fill,
-                        height: 250,
-                      ),
-                    ),
-                    Positioned(
-                      left: 180,
-                      top: 260,
-                      child: Image.asset('assets/snake9.png'),
-                    ),
-                    Positioned(
-                      left: 190,
-                      bottom: 20,
-                      child: Image.asset('assets/snake10.png'),
-                    ),
-                    Positioned(
-                      left: 370,
-                      bottom: 70,
-                      child: Image.asset(
-                        'assets/snake11.png',
-                        fit: BoxFit.fill,
-                        height: 150,
-                      ),
-                    ),
-                  ],
+                return TabuleiroWidget(
+                  tabuleiro: (Tabuleiro(10, 10, _data as List<Campo>)),
                 );
               }
             },
