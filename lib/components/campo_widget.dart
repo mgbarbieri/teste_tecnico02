76323683a8bbfd7c6c570f70e03810790ae090ab
Matheus Barbieri <mgbarbieri20@gmail.com>
2022-01-26 19:11:38 -0300

    refactor: nova representacao campo

--- lib/components/campo_widget.dart
+++ lib/components/campo_widget.dart
@@ -3,15 +3,20 @@ import 'package:teste_tecnico02/models/campo.dart';
 
 class CampoWidget extends StatelessWidget {
   final Campo campo;
+  final Color cor;
 
-  const CampoWidget({Key? key, required this.campo}) : super(key: key);
+  const CampoWidget({Key? key, required this.campo, required this.cor})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
-      height: 5,
-      width: 5,
+      child: Text(
+        campo.numero.toString(),
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      decoration:
+          BoxDecoration(border: Border.all(color: Colors.black), color: cor),
     );
   }
 }
